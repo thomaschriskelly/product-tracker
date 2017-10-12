d39b19fbd178eccd526d832ad69dec75f3b6bbb9
Tom Kelly <thomaschriskelly@gmail.com>
2017-10-12 19:36:08 -0400

    initial index template + populated fixture

--- producttracker/tracker/views.py
+++ producttracker/tracker/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from . import models
 
-# Create your views here.
+def index(request):
+    context = {
+        'products': models.Product.objects.all(),
+        'breadcrumbs': models.Breadcrumb.objects.all(),
+    }
+    return render(request, 'index.html', context)
+
