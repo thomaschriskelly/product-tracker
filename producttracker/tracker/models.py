c495b0f67c2ae64ddd8b0d4e7b6800f31ffcf828
Tom Kelly <thomaschriskelly@gmail.com>
2017-10-12 15:31:22 -0400

    Added tracker app as well as two models for tracking products

---
+++ producttracker/tracker/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+class Product(models.Model):
+    description = models.CharField(max_length=64)
+
+class Breadcrumb():
+    product = models.ForeignKey(Product)
+    datetime = models.DateTimeField()
+    longitude = models.CharField(max_length=32)
+    latitude = models.CharField(max_length=32)
+    elevation = models.CharField(max_length=32)
