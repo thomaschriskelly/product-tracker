4bf9c28c9278f2b7d413f08192aa39c9fcd38ccf
Tom Kelly <thomaschriskelly@gmail.com>
2017-10-13 16:47:49 -0400

    location form added

--- producttracker/tracker/forms.py
+++ producttracker/tracker/forms.py
@@ -1,4 +1,12 @@
 from django import forms
+from . import models
 
 class ProductForm(forms.Form):
-    description = forms.CharField(label='', max_length=32)
+    description = forms.CharField(max_length=32)
+
+class LocationForm(forms.Form):
+    product = forms.ModelChoiceField(queryset=models.Product.objects.all())
+    datetime = forms.DateTimeField(label='Datetime (eg: 2016-10-13 14:30:59):')
+    longitude = forms.CharField(max_length=32)
+    latitude = forms.CharField(max_length=32)
+    elevation = forms.CharField(max_length=32)
--- producttracker/tracker/views.py
+++ producttracker/tracker/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
+from django.urls import reverse
 from . import models
 from . import forms
 
@@ -22,5 +23,14 @@ def locations(request, product_id):
     context = {
         'product': product,
         'breadcrumbs': models.Breadcrumb.objects.filter(product=product),
+        'location_form': forms.LocationForm(),
     }
     return render(request, 'locations.html', context)
+
+def location(request):
+    if request.method == 'POST':
+        form = forms.LocationForm(request.POST)
+        if form.is_valid():
+            models.Breadcrumb.objects.create(**form.cleaned_data)
+            redirect_url = reverse('locations', args=(form.cleaned_data['product'].id,))
+            return HttpResponseRedirect(redirect_url)
